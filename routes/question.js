5e6f56469e02cdbd5bf36435b52a4f8811046921
Mehul Patel <hello@differentstudios.us>
2017-01-04 17:33:58 -0500

    updated to prod

--- routes/question.js
+++ routes/question.js
@@ -1,27 +1,27 @@
-var BASE_URL = "https://o2uzqo7yv2.execute-api.us-east-1.amazonaws.com/test/";
+var BASE_URL = "https://o2uzqo7yv2.execute-api.us-east-1.amazonaws.com/prod/";
 var QUESTION_INFO_URL = BASE_URL + "get_question_info?question_id=";
 var S3_URL = BASE_URL + "/get_s3_get_url_web?type=";
 var QUESTION = "question";
 var ANSWER = "answer";
 var FILENAME_PARAM = "&filename=";
-
 var request = require('request');
 
 exports.display = function (req, res) {
     var id = req.params.id;
     request(QUESTION_INFO_URL + id, function(err, response) {
-        if (err) return res.render('404');
+        if (err) return res.redirect('/');
         else {
             var data = JSON.parse(response.body);
+            console.log(data);
             if (data.question != undefined) {
                 request(S3_URL + QUESTION + FILENAME_PARAM + data.question.filename, function(err, response){
-                    if (err) return res.render('404');
+                    if (err) return res.redirect('/');
                     else {
                         var question_url = JSON.parse(response.body);
                         data.question_url = question_url;
                         if (data.answer != undefined) {
                             request(S3_URL + ANSWER + FILENAME_PARAM + data.answer.filename, function(err, response){
-                                if (err) return res.render('404');
+                                if (err) return res.redirect('/');
                                 else {
                                     var answer_url = JSON.parse(response.body);
                                     data.answer_url = answer_url;
@@ -34,7 +34,7 @@ exports.display = function (req, res) {
                     }
                 });
             } else {
-                res.render('404');
+                return res.redirect('/');
             }
         }
     })
