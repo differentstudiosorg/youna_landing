b3245e4492f2378ef5d0ed9e6e323e59d83cab0f
Mehul Patel <hello@differentstudios.us>
2017-02-02 10:33:20 -0500

    fixed answers again

--- public/javascripts/access.js
+++ public/javascripts/access.js
@@ -2,10 +2,6 @@ $('document').ready(function(){
 
   $('.message').html("");
 
-  if ($(".q-answer").text().length < 100) {
-    $(".question-bottom-text").css("top", "510px");
-  }
-
   $('#text').click(function() {
       var number = $('.number').val();
       var len = number.length;
